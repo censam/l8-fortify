7c687d9776047ba04a6036aeaa76e40114b0d53d
censam <wgsampath@gmail.com>
2021-07-02 12:23:09 +0530

    apis start up

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Laravel\Fortify\Http\Controllers\RegisteredUserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +14,13 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+$limiter = config('fortify.limiters.login');
+$twoFactorLimiter = config('fortify.limiters.two-factor');
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/register', [RegisteredUserController::class, 'store'])
+->middleware(['guest:'.config('fortify.guard')]);
